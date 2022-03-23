a94496e47918162a91d3cbed6a1ed4929c644247
Bedirhan Tong <cengbedirhantong@gmail.com>
2022-03-23 16:05:31 +0300

    Lab exercise 01 added and builded

---
+++ src/com/bedirhan/undergraduate/CSE101/CSE101L/lab01/Lab1_20200808033.java
@@ -0,0 +1,9 @@
+package com.bedirhan.undergraduate.CSE101.CSE101L.lab01;
+
+public class Lab1_20200808033 {
+    public static void main(String[] args) {
+        System.out.println("Student Number : 20200808033");
+        System.out.println("Name : Bedirhan Tong");
+        System.out.println("Department: Computer Engineering");
+    }
+}
