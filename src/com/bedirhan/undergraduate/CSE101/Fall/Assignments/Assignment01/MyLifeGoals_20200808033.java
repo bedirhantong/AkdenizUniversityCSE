d921d9014f49b6f2af1a4b5c045ef63fb999aa3c
Bedirhan Tong <cengbedirhantong@gmail.com>
2022-03-23 15:54:50 +0300

    MyLifeGoals_20200808033 project added and builded

---
+++ src/com/bedirhan/undergraduate/CSE101/Fall/Assignments/Assignment01/MyLifeGoals_20200808033.java
@@ -0,0 +1,20 @@
+package com.bedirhan.undergraduate.CSE101.Fall.Assignments.Assignment01;
+
+public class MyLifeGoals_20200808033 {
+    public static void main(String[] args) {
+        System.out.println("Bedirhan Tong");
+        System.out.print("I think that the fact that students can see their progress ");
+        System.out.print("in a subject or course concretely will instill both motivat");
+        System.out.println("ion and desire to work.");
+
+        System.out.print("Instead of selling the application I developed to a famous ");
+        System.out.print("company, I want to help the development of the country by p");
+        System.out.print("resenting it with open source code to software developers");
+        System.out.println(" who want to improve themselves.");
+
+        System.out.print("It is my biggest dream to provide the independence in softw");
+        System.out.print("software and the freedom to access the desired resources to");
+        System.out.println(" the students I will train.");
+
+    }
+}
