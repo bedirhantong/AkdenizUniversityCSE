c8315a363a5bf815add7f2ec8d2a9c6cf26e73af
Bedirhan <cengbedirhantong@gmail.com>
2022-05-01 21:15:35 +0300

    Lab05 and Lab05Test added and designed

---
+++ src/com/bedirhan/undergraduate/CSE102/CSE102L/Ex07/Lab05Test.java
@@ -0,0 +1,32 @@
+package com.bedirhan.undergraduate.CSE102.CSE102L.Ex07;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class Lab05Test {
+    Triangle triangle;
+
+    @Before
+    public void setup() {
+        try {
+            triangle = new Triangle(3.0, 4.0, 5.0);
+        }
+        catch (IllegalTriangleException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldGetPerimeter() {
+
+        assertEquals(12.0, triangle.getPerimeter(), 0.001);
+    }
+
+    @Test
+    public void shouldGetArea() {
+        assertTrue(triangle.getArea() < 10);
+    }
+}
